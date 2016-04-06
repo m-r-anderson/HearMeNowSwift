cfb00bc586108b07f0b1684e3a4010fbdebc133e
Mike Anderson <MikeAnderson@mra-mac-mini.local>
2016-04-06 14:11:42 -0400

    Initial Commit

---
+++ HearMeNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HearMeNow
+//
+//  Created by Mike Anderson on 4/6/16.
+//  Copyright © 2016 Michael R. Anderson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
